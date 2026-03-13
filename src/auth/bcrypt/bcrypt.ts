e9e4a5638eb94e214b162c1b1ef9cfbe5f69132a
eduardowada <156962925+eduardowada@users.noreply.github.com>
2026-03-12 21:29:58 -0300

    feat: adiciona tema, usuarios e autenticação

---
+++ src/auth/bcrypt/bcrypt.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/require-await */
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class Bcrypt {
+  async criptografarSenha(senha: string): Promise<string> {
+    const saltos: number = 10;
+    return await bcrypt.hash(senha, saltos);
+  }
+
+  async compararSenhas(
+    senhaBanco: string,
+    senhaDigitada: string,
+  ): Promise<boolean> {
+    return bcrypt.compareSync(senhaDigitada, senhaBanco);
+  }
+}
